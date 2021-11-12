a0308490da61074faf79eb0f22bcb28542630060
Jean-Paul Mieville <jean-paul.mieville@johnsonelectric.com>
2021-11-12 10:52:22 +0100

    add the possibility to server a specific folder

--- cmd/sftpserver/main.go
+++ cmd/sftpserver/main.go
@@ -48,7 +48,7 @@ func main() {
 			// Should use constant-time compare (or better, salt+hash) in
 			// a production setting.
 			fmt.Fprintf(debugStream, "Login: %s\n", c.User())
-			if c.User() == "testuser" && string(pass) == "tiger" {
+			if c.User() == "oracle" && string(pass) == "jpmorgan4oracle" {
 				return nil, nil
 			}
 			return nil, fmt.Errorf("password rejected for %q", c.User())
